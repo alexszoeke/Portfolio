c265762a3b4371fb5fca44bc3140236069039dc4
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-06-08 12:47:35 -0500

    jawsdb config

--- models/index.js
+++ models/index.js
@@ -8,10 +8,11 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWDB_URL);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 }
 
 fs
