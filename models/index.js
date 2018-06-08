04216d5f8f2659d20a6785339892a913fa497713
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-06-08 14:11:37 -0500

    jawsdb

--- models/index.js
+++ models/index.js
@@ -8,12 +8,10 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (process.env.JawsDB_URL) {
-  var sequelize = new Sequelize(config.use_env_variable, config.dialect, config);
-  // var connection = mysql.createConnection(process.env.JawsDB_URL);
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-  // var sequelize = new Sequelize(process.env[config.use_env_variable], config);
 }
 
 fs
