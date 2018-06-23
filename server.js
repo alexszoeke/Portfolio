62bd006a6c23f340352b35cea11d58a1d871cb03
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-06-23 11:41:46 -0500

    contact update

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.set("view engine", "handlebars");
 
 // Requiring our routes
 require("./controllers/html-routes.js")(app);
-require("./controllers/api-routes.js")(app);
+// require("../api-routes")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({force: true}).then(function () {
+// db.sequelize.sync({force: true}).then(function () {
     app.listen(PORT, function () {
         console.log("Server listening on: http://localhost:" + PORT);
     });
-  });
+//   });
