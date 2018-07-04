af5fab94db036553b21be3cba11db0c1a2a5cdfb
Alex Szoeke <alex_szoeke@outlook.com>
2018-07-04 10:31:47 -0500

    optimized photos, added to portfolio

--- public/javascript/contact.js
+++ public/javascript/contact.js
@@ -30,18 +30,57 @@ $(document).ready(function () {
     //         window.location.href = "/contact";
     //     });
 
-    var data = {
-        name: $("#name").val(),
-        email: $("#email").val(),
-        message: $("#message").val()
-    };
-    $.ajax({
-        type: "POST",
-        url: $("form").attr('action'),
-        data: data,
-        success: function(){
-            $('.success').fadeIn(1000);
-        }
-    });
+    // var data = {
+    //     name: $("#name").val(),
+    //     email: $("#email").val(),
+    //     message: $("#message").val()
+    // };
+    // $.ajax({
+    //     type: "POST",
+    //     url: $("form").attr('action'),
+    //     data: data,
+    //     success: function(){
+    //         $('.success').fadeIn(1000);
+    //     }
+    // });
+
+    $("#contact-form").click(function() {
+        var name = $("#name").val().trim();
+        var email = $("#email").val().trim();
+        var message = $("#message").val().trim();
+        console.log(name);
+        console.log(email);
+        console.log(message);
+
+
+        var form = {
+            name: name,
+            email: email,
+            message: message
+        };
+        $.ajax({
+            type: “POST”,
+            url: "mandrillapp.com/api/1.0/messages/send.json",
+            data: {
+              ‘key’: ‘3OFrXm32FEuJs_wkCqqlzQ’,
+              ‘message’: {
+                ‘from_email’: "alex_szoeke@outlook.com’,
+                ‘to’: [
+                    {
+                      ‘email’: ‘alexszoeke@gmail.com’,
+                      ‘type’: ‘to’
+                    }
+                  ],
+                ‘autotext’: ‘true’,
+                ‘subject’: ‘New Website Email’,
+                ‘html’: form,
+              }
+            }
+           }).done(function(response) {
+             console.log(response); // if you're into that sorta thing
+           });
+    })
+
+    
 
 });
